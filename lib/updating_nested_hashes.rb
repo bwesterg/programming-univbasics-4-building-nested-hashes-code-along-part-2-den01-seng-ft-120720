f351643b0ee0b2ba3b99243042e661df29a644c4
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-21 23:12:40 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -27,14 +27,14 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
  
 end
 
-def add_characters
-   epic_tragedy = {
-      :montague => {
-         :patriarch => {name: "Lord Montague", age: "53"},
-         :matriarch => {name: "Lady Montague", age: "54"},
-         :hero => {name: "Romeo", age: "15", status: "alive"},
-         :hero_friends => [
-            {name: "Benvolio", age: "17", attitude: "worried"},
+# def add_characters
+#   epic_tragedy = {
+ #     :montague => {
+  #       :patriarch => {name: "Lord Montague", age: "53"},
+   #      :matriarch => {name: "Lady Montague", age: "54"},
+    #     :hero => {name: "Romeo", age: "15", status: "alive"},
+     #    :hero_friends => [
+     #       {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
          ]
       },
