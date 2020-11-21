5776a3efeb899c9ba6d5e6386c567bce5af59742
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-21 23:25:10 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -28,30 +28,28 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
 
 
-epic_tragedy[:montague][:h
 
-
-# def add_characters
-#   epic_tragedy = {
- #     :montague => {
-  #       :patriarch => {name: "Lord Montague", age: "53"},
-   #      :matriarch => {name: "Lady Montague", age: "54"},
-    #     :hero => {name: "Romeo", age: "15", status: "alive"},
-     #    :hero_friends => [
-     #       {name: "Benvolio", age: "17", attitude: "worried"},
-  #          {name: "Mercutio", age: "18", attitude: "hot-headed"}
-  #       ]
-  #    },
-  #    :capulet => {
-  #       :patriarch => {name: "Lord Capulet", age: "50"},
-  #       :matriarch => {name: "Lady Capulet", age: "51"},
-   #      :heroine => {name: "Juliet", age: "15", status: "alive"}#,
- #        :heroine_friends => [
- #        {name: "Steven", age: "30", attitude: "confused"},
- #        {name: "Nurse", age: "44", attitude: "worried"}
-  #       ]
-  #    }
-#   }
+def add_characters
+   epic_tragedy = {
+      :montague => {
+         :patriarch => {name: "Lord Montague", age: "53"},
+         :matriarch => {name: "Lady Montague", age: "54"},
+         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero_friends => [
+            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         ]
+      },
+      :capulet => {
+         :patriarch => {name: "Lord Capulet", age: "50"},
+         :matriarch => {name: "Lady Capulet", age: "51"},
+         :heroine => {name: "Juliet", age: "15", status: "alive"}#,
+         :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}
+         ]
+      }
+   }
 
    # Write code below that does the following programmatically:
    #
@@ -67,5 +65,5 @@ epic_tragedy[:montague][:h
 
 
    # After your code, the following line will return the altered hash
- #  epic_tragedy
-#end
+   epic_tragedy
+end
