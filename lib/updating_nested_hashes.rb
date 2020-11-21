22b5794fb7739bcf88aa2399e85d3fd900dc7211
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-21 23:49:50 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -23,9 +23,8 @@ def update_status
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-epic_tragedy[:montague][:hero][:status]
-epic_tragedy[:capulet][:heroine][:status]
 
+epic_tragedy
 
  
 end
