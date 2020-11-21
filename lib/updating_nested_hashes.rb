b91f3066de7b83cde35d8c3ed12c69287ba42a3c
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-21 23:13:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -41,13 +41,13 @@ end
   #    :capulet => {
   #       :patriarch => {name: "Lord Capulet", age: "50"},
   #       :matriarch => {name: "Lady Capulet", age: "51"},
-   #      :heroine => {name: "Juliet", age: "15", status: "alive"},
-         :heroine_friends => [
-         {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
-      }
-   }
+   #      :heroine => {name: "Juliet", age: "15", status: "alive"}#,
+ #        :heroine_friends => [
+ #        {name: "Steven", age: "30", attitude: "confused"},
+ #        {name: "Nurse", age: "44", attitude: "worried"}
+  #       ]
+  #    }
+#   }
 
    # Write code below that does the following programmatically:
    #
@@ -63,5 +63,5 @@ end
 
 
    # After your code, the following line will return the altered hash
-   epic_tragedy
+ #  epic_tragedy
 end
